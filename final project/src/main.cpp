38da2f683e806aa679d687c36fce9a7454e248b5
meteors27 <williamxia27@qq.com>
2023-06-18 02:52:03 +0800

    modify `main.cpp`

--- final project/src/main.cpp
+++ final project/src/main.cpp
@@ -1,44 +1,6 @@
-#include "XMLSerialization.hpp"
 #include <iostream>
-#include <string>
-#include <vector>
-struct DDL
-{
-    std::string name;
-    int left_days;
-    std::vector<std::string> ref_books;
-
-    void serialize(const std::string &filename)
-    {
-        XMLSerialization::serializer serializer(filename, "ddl");
-        serializer.serialize(name);
-        serializer.serialize(left_days);
-        serializer.serialize(ref_books);
-    }
-
-    void deserialize(const std::string &filename)
-    {
-        XMLSerialization::deserializer deserializer(filename, "ddl");
-        deserializer.deserialize(name);
-        deserializer.deserialize(left_days);
-        deserializer.deserialize(ref_books);
-    }
-
-    bool operator==(const DDL &other) const
-    {
-        return name == other.name && left_days == other.left_days && ref_books == other.ref_books;
-    }
-};
 
 int main()
 {
-    // // question: 模版函数的匹配
-    // // 错误处理
-    DDL ddl1 = {"C++ Primer", 10, {"C++ Primer", "Effective C++"}}, ddl2;
-    ddl1.serialize("ddl.xml");
-    ddl2.deserialize("ddl.xml");
-    std::cout << (ddl1 == ddl2
-                      ? "true"
-                      : "false")
-              << std::endl;
+    std::cout << "所有的测试文件都在test目录下，如果能过编译，那么所有的测试都已经跑通" << std::endl;
 }
